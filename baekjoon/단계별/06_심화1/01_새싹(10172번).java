e9ac646cf4160d324819032b5764537c5636fd2c
トロ <dkanehahfma365@gmail.com>
2026-05-04 14:18:08 +0900

    Create 01_새싹(10172번).java

---
+++ baekjoon/단계별/06_심화1/01_새싹(10172번).java
@@ -0,0 +1,23 @@
+/*
+백준 10172번 - 새싹
+날짜: 2026/2/27 (금) | 소요시간: 5분
+
+[접근 방식]
+어려운 부분 없음
+중간에 공백 실수로 한번 틀림
+
+# ai와 풀이 방식 일치
+*/
+
+// 입력: 입력은 없다.
+// 출력: 새싹을 출력한다.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
