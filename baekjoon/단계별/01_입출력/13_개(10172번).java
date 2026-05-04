be2c7185c8cc1c83befa1dc112f636d2cdeb5670
トロ <dkanehahfma365@gmail.com>
2026-05-04 13:40:12 +0900

    Create 13_개(10172번).java

---
+++ baekjoon/단계별/01_입출력/13_개(10172번).java
@@ -0,0 +1,18 @@
+/*
+백준 10172번 - 개
+날짜: 2025/12/29 (월) | 소요시간: 5분
+
+[접근 방식]
+특수한 규칙이 없어서 생으로 구현하는게 맞다 생각
+딱히 고민한 부분은 없음
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
