0fb6d34c03323359ab346977a691f5325beb7ece
トロ <dkanehahfma365@gmail.com>
2026-05-07 07:33:32 +0900

    Update and rename 07_팔(1105번) to 07_팔(1105번).java

--- baekjoon/단계별/08_수학1/07_팔(1105번).java
+++ baekjoon/단계별/08_수학1/07_팔(1105번).java
@@ -4,8 +4,8 @@
 // 소요시간 30분 ~
 // L ~ R 사이에 8이 몇 개 있냐? 를 구하는 줄알았는데, 
 // L ~ R 사이 수 중에 8이 가장 적게 들어있는 수를 찾고 (L또는 R에 8이 반드시 포함됨)
-// L과 R이 8이 포함되는 숫자인지 파악 
-// 포함 되면, 그 사이 8이 포함된 수가 몇개 나오는지 최솟값 구함 
+/// L과 R이 8이 포함되는 숫자인지 파악 
+/// 포함 되면, 그 사이 8이 포함된 수가 몇개 나오는지 최솟값 구함 
 
 import java.io.*; 
 
@@ -19,8 +19,10 @@ public class Main {
         String R = arr[1]; 
         int result = 0; 
 
-        if(L.contains("8") || R.contains("8")){
-            System.out.println("Hello, World!");
+        for (int i = 0; i< L.length(); i++){
+            if (L.charAt(i) == '8' && R.charAt(i) == '8' ){
+                result ++; 
+            }
         }
         System.out.println(result);
     }
