60e65e74851e8be1ab68e99f816205ccba29ad17
トロ <dkanehahfma365@gmail.com>
2026-05-13 09:38:50 +0900

    Update 08_ 황소 다바고치.java

--- baekjoon/단계별/08_수학1/08_ 황소 다바고치.java
+++ baekjoon/단계별/08_수학1/08_ 황소 다바고치.java
@@ -1,6 +1,5 @@
 /* 
-5/12(화) | Silver II 
-소요 시간 30분 ~ 
+5/12(화) ~ 13(수) | Silver II  소요시간 1시간 
    입력: 첫 번째 줄에 테스트 케이스의 갯수 T가 주어진다.
    두 번째 줄에 황소의 초기 체력을 나타내는 정수 n 과, 현재 가지고 있는 먹이의 갯수 m 입력
    출력: 황소가 며칠째 밤에 죽게 되는지 출력
@@ -21,19 +20,20 @@ public class Main {
         // Buffered 선언 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 
-        // T, M(먹이 수), N 변수 선언 
+        // T 변수 선언 
         int T = Integer.parseInt(br.readLine()); 
-        String arr[] = br.readLine().split(" "); 
-        int N = Integer.parseInt(arr[0]); 
-        int M = Integer.parseInt(arr[1]); 
 
         for (int i = 1; i <= T; i++ ){
-            System.out.println(i);
-            for (int j =N; j<0; j/=2){
-                j +=1;
-                        System.out.println("Hello, World!");
+            // M(먹이 수), N 변수 선언 
+            String arr[] = br.readLine().split(" "); 
+            long N = Long.parseLong(arr[0]); 
+            long M = Long.parseLong(arr[1]); 
+            int count =0; 
+    
+            for (long j =N; j>0; j/=2){
+                count ++ ;
             }
+            System.out.println(count+ M);
         }
     }
 }
-// 8589934591 1
