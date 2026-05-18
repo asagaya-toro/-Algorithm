b52b23ad369a435e32589e1ebcc038214efc2b17
トロ <dkanehahfma365@gmail.com>
2026-05-19 07:22:47 +0900

    [Passed] 달에서 무게 구하기 (45ms, 16MB)

---
+++ Codetree/trail0/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,10 @@
+# 출력 : 13 * 0.165000 = 2.145000
+# 날짜: 2026/5/19 (화) 
+# 난이도 : 쉬움 | 소요시간: 1분
+
+# 변수 선언 
+weight = 13 
+moon_gravity_ratio = 0.165
+
+#  출력 
+print(f"{weight} * {moon_gravity_ratio:.6f} = {weight*moon_gravity_ratio:.6f}")
